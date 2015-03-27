e20317f0582a73f7c56cf883c11f465a056c0fb3
Hideaki SAWADA <chuno@live.jp>
2015-03-27 12:10:35 +0900

    Update lang.php

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -1,9 +1,10 @@
 <?php
 /**
-* japanese language file
-*
 * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*
+* @author Hideaki SAWADA <chuno@live.jp>
 */
+
 $lang['discussion'] = 'コメント';
 $lang['back_to_article'] = '記事へ戻る';
 $lang['userpage'] = 'ユーザーページ';
