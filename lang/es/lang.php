cd5abbdd0f7a846106f52264961735b47dc8d366
Nelson Martell <nelson6e65-github@yahoo.es>
2015-03-27 16:55:01 -0430

    add: Spanish translation for lang.php
    Spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Spanish language file for template
+ *
+ */
+$lang['discussion']        = 'Discusión';
+$lang['back_to_article']   = 'Volver al artículo';
+$lang['userpage']          = 'Página de Usuario';
