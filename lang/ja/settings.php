49cd2665ddd2e76481007bef5cc85e6f26680a63
Hideaki SAWADA <chuno@live.jp>
2015-03-27 12:03:35 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* japanese language file
+*
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*/
+$lang['inverseNavbar'] = 'navbar の色を反転する';
+$lang['fixedTopNavbar'] = 'navbar を上部に固定する';
+$lang['bootstrapTheme'] = 'テーマの選択（default Bootstrap テーマ・Bootstrap optional テーマ・Bootswatch.com テーマ・ custom テーマ）';
+$lang['customTheme'] = 'custom テーマの URL';
+$lang['bootswatchTheme'] = 'Bootswatch.com テーマの選択';
+$lang['showThemeSwitcher'] = 'navbar 内に Bootswatch.com テーマ切替を表示する';
