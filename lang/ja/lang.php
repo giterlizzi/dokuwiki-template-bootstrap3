7d7980063cde84ef44007320bf74fadc158a6370
Hideaki SAWADA <chuno@live.jp>
2015-03-27 12:03:01 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* japanese language file
+*
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*/
+$lang['discussion'] = 'コメント';
+$lang['back_to_article'] = '記事へ戻る';
+$lang['userpage'] = 'ユーザーページ';
