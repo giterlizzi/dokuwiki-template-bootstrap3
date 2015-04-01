3ba9049b9617f9e5c95e2f56294df0efc09b031a
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-04-01 21:53:49 +0200

    Added license in English translation

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,6 +2,8 @@
 /**
  * English language file for template
  *
+ * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
 $lang['discussion']        = 'Discussion';
