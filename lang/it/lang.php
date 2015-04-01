27fb5e4b52d78f000f1cb2ff3ed3307b1a9134ee
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-04-01 21:54:09 +0200

    Added Italian translation

---
+++ lang/it/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Italian language file for template
+ *
+ * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['discussion']        = 'Discussione';
+$lang['back_to_article']   = 'Ritorna all\'articolo';
+$lang['userpage']          = 'Pagina Utente';
---
+++ lang/it/settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Italian language file for config
+ *
+ * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['showTranslation']   = 'Mostra la barra delle traduzioni (richiede <em>Translation Plugin</em>)';
+$lang['inverseNavbar']     = 'Barra di naginazione (navbar) invertita';
+$lang['fixedTopNavbar']    = 'Blocca la barra di navigazione in alto';
+$lang['bootstrapTheme']    = 'Seleziona un tema (tema di default Bootstrap, tema opzionale di Bootstrap, temi da Bootswatch.com oppure un tema custom)';
+$lang['customTheme']       = 'Inserisci l\'URL del tema custom';
+$lang['bootswatchTheme']   = 'Seleziona un tema di Bootswatch.com';
+$lang['showThemeSwitcher'] = 'Mostra il selettore dei temi di Bootswatch.com nella barra di navigazione';
