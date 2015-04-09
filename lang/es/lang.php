299fd88a9268d23e54199b6458fd6c9edb96c87e
Nelson Martell <nelson6e65-github@yahoo.es>
2015-04-08 23:57:27 -0430

    Update lang.php

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * Spanish language file for template
- *
+ * @author   Nelson Martell <nelson6e65-github@yahoo.es>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 $lang['discussion']        = 'Discusión';
 $lang['back_to_article']   = 'Volver al artículo';
