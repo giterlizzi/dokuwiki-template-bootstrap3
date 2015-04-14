5fbc5c5983fee472580f01d10932732900704aeb
Hideaki SAWADA <chuno@live.jp>
2015-04-14 10:42:19 +0900

    Update settings.php

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -4,10 +4,19 @@
 *
 * @author Hideaki SAWADA <chuno@live.jp>
 */
-
-$lang['inverseNavbar'] = 'navbar の色を反転する';
-$lang['fixedTopNavbar'] = 'navbar を上部に固定する';
-$lang['bootstrapTheme'] = 'テーマの選択（default Bootstrap テーマ・Bootstrap optional テーマ・Bootswatch.com テーマ・ custom テーマ）';
-$lang['customTheme'] = 'custom テーマの URL';
-$lang['bootswatchTheme'] = 'Bootswatch.com テーマの選択';
-$lang['showThemeSwitcher'] = 'navbar 内に Bootswatch.com テーマ切替を表示する';
+$lang['showTools']              = 'navbar にツールを表示する';
+$lang['showTools_o_never']      = '表示しない';
+$lang['showTools_o_logged']     = 'ログインしたときに表示する';
+$lang['showTools_o_always']     = '常に表示する';
+$lang['sidebarPosition']        = 'DokuWiki サイドバーの配置（<code>left</code> 又は <code>right</code>）';
+$lang['rightSidebar']           = '<strong>EXPERIMENTAL</strong> 右サイドバーのページ名。空欄で右サイドバー無効。デフォルトの DokuWiki <a href="#config___sidebar">サイドバー</a>が有効で <code>left</code> に配置している場合（<a href="#config___tpl____bootstrap3____sidebarPosition">
+tpl»bootstrap3»sidebarPosition</a> 設定を参照）のみ右サイドバーを表示します。DokuWiki サイドバーを右側に配置したい場合、<a href="#config___tpl____bootstrap3____sidebarPosition">
+tpl»bootstrap3»sidebarPosition</a> 設定を <code>right</code> にしてください。';
+$lang['showTranslation']        = '翻訳ツールバーを表示する（<em>Translation プラグイン</em>が必要）';
+$lang['inverseNavbar']          = 'navbar の色を反転する';
+$lang['fixedTopNavbar']         = 'navbar を上部に固定する';
+$lang['bootstrapTheme']         = 'テーマの選択（default Bootstrap テーマ・Bootstrap optional テーマ・Bootswatch.com テーマ・ custom テーマ）';
+$lang['customTheme']            = 'custom テーマの URL';
+$lang['bootswatchTheme']        = 'Bootswatch.com テーマの選択';
+$lang['showThemeSwitcher']      = 'navbar 内に Bootswatch.com テーマ切替を表示する';
+$lang['showPageInfo']           = 'ページのメタデータを表示する（日付、編集者など）';
