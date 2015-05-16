e4f21494887893a1d076c0fba876ff94cfa95955
insspb <ashpak@ashpak.ru>
2015-05-16 15:39:33 +0300

    Russian language translation
    Fully translated. Some text changed to adapted.

---
+++ lang/ru/settings.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Russian language file for config
+ *
+ * @author   Andrey Shpak <ashpak@ashpak.ru>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+ 
+$lang['discussionPage']                        = 'Ссылка на страницу обсуждения (по умолчанию<code>discussion:@ID@</code>, где <code>@ID@</code> автоматически заменяется именем текущей страницы), пустое поле выключает функционал';
+$lang['showTools']                             = 'Отображение инструментов в панели навигации';
+$lang['showTools_o_never']                     = 'Никогда';
+$lang['showTools_o_logged']                    = 'Для вошедших пользователей';
+$lang['showTools_o_always']                    = 'Всегда';
+$lang['sidebarPosition']                       = 'Позиция боковой панели DokuWiki (<code>слева</code> или <code>справа</code>)';
+$lang['sidebarPosition_o_right']               = 'справа';
+$lang['sidebarPosition_o_left']                = 'слева';
+$lang['rightSidebar']                          = 'Имя страницы правой боковой панели, пустое поле отключает панель. Правая боковая панель отображается только если <a href="#config___sidebar">боковая панель</a> Dokuwiki по умолчанию включена и находится <code>слева</code>. (Смотреть <a href="#config___tpl____bootstrap3____sidebarPosition">tpl»bootstrap3»sidebarPosition</a>). Если вы хотите видеть панель навигации Dokuwiki по умолчанию спава, установите <code>справа</code> в <a href="#config___tpl____bootstrap3____sidebarPosition">tpl»bootstrap3»sidebarPosition</a>';
+$lang['tableFullWidth']                        = 'Разрешить таблицы в 100% ширину';
+$lang['semantic']                              = 'Включить семантические данные Schema.org. Для полной поддержи требуется установка <a href="https://www.dokuwiki.org/plugin:semantic">плагина</a>';
+$lang['schemaOrgType']                         = 'Тип Schema.org данных';
+$lang['showTranslation']                       = 'Отображать языковую панель (требует установки <a href="https://www.dokuwiki.org/plugin:Translation">плагина</a>';
+$lang['inverseNavbar']                         = 'Перевернуть панель навигации';
+$lang['fixedTopNavbar']                        = 'Зафиксировать панель навигации сверху';
+$lang['fluidContainer']                        = 'Разрешить плавающий контейнер(страница во весь экран)';
+$lang['pageOnPanel']                           = 'Включить рамку вокруг страницы';
+$lang['bootstrapTheme']                        = 'Выберите графическую тему (Bootstrap оригинальная, Bootstrap дополнительная, темы Bootswatch.com или графическая тема по выбору)';
+$lang['bootstrapTheme_o_default']              = 'Bootstrap оригинальная';
+$lang['bootstrapTheme_o_default-optional']     = 'Bootstrap дополнительная';
+$lang['bootstrapTheme_o_custom']               = 'Графическая тема по выбору';
+$lang['bootstrapTheme_o_bootswatch']           = 'Темы Bootswatch.com';
+$lang['customTheme']                           = 'Вставьте адрес графической темы по выбору';
+$lang['bootswatchTheme']                       = 'Выберите графическую тему Bootswatch.com';
+$lang['showThemeSwitcher']                     = 'Отображать выбор тем Bootswatch.com в панели навигации';
+$lang['showPageInfo']                          = 'Отображать информацию о странице (например, дата, автор)';
+$lang['showBadges']                            = 'Отображать значки внизу страницы (DokuWiki, Donate, и т.д.)';
