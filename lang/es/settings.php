b197120fc72756d190ab8d3c388748601b4aabb2
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-05-19 21:05:46 -0430

    add: Missing elements and translated to spanish.

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -5,6 +5,9 @@
  * @author   Nelson Martell <nelson6e65-github@yahoo.es>
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
+
+$lang['discussionPage']     = 'Nombre de la página de discusión (por defecto es <code>discussion:@ID@</code>, en donde el marcador de posición <code>@ID@</code> reemplaza el nombre actual de la página), el campo vacío deshabilita el enlace';
+$lang['showDiscussion']     = 'Muestra el enlace de discusión en el menú de herramientas';
 $lang['showTools']          = 'Mostrar Herramientas en la barra de navegación';
 $lang['showTools_o_never']  = 'Nunca';
 $lang['showTools_o_logged'] = 'Cuando está identificado';
@@ -13,11 +16,21 @@ $lang['sidebarPosition']    = 'Posición de la barra lateral DokuWiki (<code>lef
 $lang['rightSidebar']       = 'El nombre de la página en la Barra Lateral a la Derecha, el campo vacío deshabilita la barra lateral derecha. La Barra Lateral Derecha sólo es mustrada cuando la <a href="#config___sidebar">barra lateral</a> predeterminada está habilitada y está sobre la posición <code>izquierda</code> (ver la configuración <a href="#config___tpl____bootstrap3____sidebarPosition">
 tpl»bootstrap3»sidebarPosition</a>). Si sólo quieres la barra lateral de DokuWiki en la posición derecha, establece configuración <a href="#config___tpl____bootstrap3____sidebarPosition">
 tpl»bootstrap3»sidebarPosition</a> con el valor <code>right</code> (derecha)';
+$lang['tableFullWidth']     = 'Habilitar el ancho de tabla a 100% (predeterminado en Bootstrap)';
+$lang['semantic']           = 'Habilitar datos semánticos';
+$lang['schemaOrgType']      = 'Tipo de Schema.org (<code>Article</code>, <code>NewsArticle</code>, <code>TechArticle</code>, <code>BlogPosting</code>)';
 $lang['showTranslation']    = 'Mostrar barra de traducción (requiere <em>Translation Plugin</em>)';
 $lang['inverseNavbar']      = 'Barra de navegación (navbar) invertida';
 $lang['fixedTopNavbar']     = 'Fijar la barra de navegación (navbar) a la parte de arriba';
-$lang['bootstrapTheme']     = 'Selecciona un tema (tema default de Bootstrap, tema optional de Bootstrap, temas Bootswatch.com o un tema personalizado)';
-$lang['customTheme']        = 'Inserta la URL del tema personalizado (custom)';
+$lang['fluidContainer']     = 'Habilitar el contenedor fluído (todo el ancho de la página)';
+$lang['pageOnPanel']        = 'Habilitar el panel alrededor de la página';
+$lang['bootstrapTheme']     = 'Tema de Bootstrap';
+$lang['bootstrapTheme_o_default']    = 'Tema Vanilla de Bootstrap';
+$lang['bootstrapTheme_o_optional']   = 'Tema opcional de Bootstrap';
+$lang['bootstrapTheme_o_custom']     = 'Tema personalizado de Bootstrap theme';
+$lang['bootstrapTheme_o_bootswatch'] = 'Tema de Bootswatch.com';
+$lang['customTheme']        = 'Inserta la URL del tema personalizado';
 $lang['bootswatchTheme']    = 'Selecciona un tema de Bootswatch.com';
 $lang['showThemeSwitcher']  = 'Mostrar selector de temas Bootswatch.com en la barra de navegación';
 $lang['showPageInfo']       = 'Mostrar información de la página (por ejemplo, fecha, autor)';
+$lang['showBadges']         = 'Mostrar botones insignia (DokuWiki, Donar, etc)';
