032ba06f85590e34b5a9285f742cd15d9a1adb38
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-06-23 15:36:23 +0200

    Added Simplified Chinese translation (thanks to dev@openbdh.com)

---
+++ lang/zh/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Simplified Chinese language file for template
+ *
+ * @author   OpenBDH <dev@openbdh.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['discussion']        = '讨论';
+$lang['back_to_article']   = '回到文章';
+$lang['userpage']          = '用户页';
+$lang['themes']            = '主题';
