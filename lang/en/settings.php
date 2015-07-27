d163e5d430ea4d170d7caa812c2b40fbbbe33db0
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-07-27 23:41:39 +0200

    Initial support for Transifex translation platform (thanks to @nelson6e65 for the idea)

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * English language file for config
+ * Language file for config
  *
  * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
