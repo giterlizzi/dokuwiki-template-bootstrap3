c2018952190236bcb23a0cf6cd7e6493a315fc57
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-12-22 14:55:15 +0100

    Improvements in tpl_badges.php

--- tpl_badges.php
+++ tpl_badges.php
@@ -11,32 +11,32 @@
 if (!defined('DOKU_INC')) die();
 
 ?>
-<?php if ($showBadges): ?>
+<?php if (bootstrap3_conf('showBadges')): ?>
 <div class="text-center hidden-print">
   <p id="dw__license">
     <?php 
       tpl_license('');
-      $target = ($conf['target']['extern']) ? 'target="'.$conf['target']['extern'].'"' : '';
+      $target   = ($conf['target']['extern']) ? 'target="'.$conf['target']['extern'].'"' : '';
+      $dw_path = dirname(tpl_basedir());
     ?>
   </p>
   <p id="dw__badges">
     <?php tpl_license('button', true, false, false); // license button, no wrapper ?>
-    <a href="http://getbootstrap.com" title="Built with Bootstrap 3" <?php echo $target?>>
+    <a href="http://getbootstrap.com" title="Built with Bootstrap 3" <?php echo $target ?>>
       <img src="<?php echo tpl_basedir(); ?>images/button-bootstrap3.png" width="80" height="15" alt="Built with Bootstrap 3" />
     </a>
-    <a href="http://www.php.net" title="Powered by PHP" <?php echo $target?>>
-      <img src="<?php echo dirname(tpl_basedir()); ?>/dokuwiki/images/button-php.gif" width="80" height="15" alt="Powered by PHP" />
+    <a href="http://www.php.net" title="Powered by PHP" <?php echo $target ?>>
+      <img src="<?php echo $dw_path ?>/dokuwiki/images/button-php.gif" width="80" height="15" alt="Powered by PHP" />
     </a>
-    <a href="http://validator.w3.org/check/referer" title="Valid HTML5" <?php echo $target?>>
-      <img src="<?php echo dirname(tpl_basedir()); ?>/dokuwiki/images/button-html5.png" width="80" height="15" alt="Valid HTML5" />
+    <a href="http://validator.w3.org/check/referer" title="Valid HTML5" <?php echo $target ?>>
+      <img src="<?php echo $dw_path ?>/dokuwiki/images/button-html5.png" width="80" height="15" alt="Valid HTML5" />
     </a>
-    <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" title="Valid CSS" <?php echo $target?>>
-      <img src="<?php echo dirname(tpl_basedir()); ?>/dokuwiki/images/button-css.png" width="80" height="15" alt="Valid CSS" />
+    <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" title="Valid CSS" <?php echo $target ?>>
+      <img src="<?php echo $dw_path ?>/dokuwiki/images/button-css.png" width="80" height="15" alt="Valid CSS" />
     </a>
-    <a href="http://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target?>>
-      <img src="<?php echo dirname(tpl_basedir()); ?>/dokuwiki/images/button-dw.png" width="80" height="15" alt="Driven by DokuWiki" />
+    <a href="http://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target ?>>
+      <img src="<?php echo $dw_path ?>/dokuwiki/images/button-dw.png" width="80" height="15" alt="Driven by DokuWiki" />
     </a>
   </p>
 </div>
 <?php endif; ?>
-        
