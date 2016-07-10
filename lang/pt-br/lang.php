8905fcda87febf97c52fefccf7e29d4c7758b24a
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-07-10 16:06:13 +0200

    Added Portuguese (Brazil) (pt-br) translation from Transifex

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Portuguese (Brazil) Language file for template
+ *
+ * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @author   Sergio Motta
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['discussion']           = 'Discussão';
+$lang['back_to_article']      = 'Voltar ao artigo';
+$lang['userpage']             = 'Página do usuário';
+$lang['themes']               = 'Temas';
+$lang['expand_container']     = 'Expandir/Reduzir';
+$lang['administrative_tasks'] = 'Tarefas Administrativas';
+$lang['additional_plugins']   = 'Plugins Adicionais';
+
+// Configuration Manager Template Sections
+$lang['config_theme']            = 'Theme';
+$lang['config_sidebar']          = 'Sidebar';
+$lang['config_navbar']           = 'Navbar';
+$lang['config_semantic']         = 'Semantic';
+$lang['config_layout']           = 'Layout';
+$lang['config_toc']              = 'TOC';
+$lang['config_discussion']       = 'Discussion';
+$lang['config_cookie_law']       = 'Cookie Law';
+$lang['config_google_analytics'] = 'Google Analytics';
+$lang['config_browser_title']    = 'Browser Title';
+$lang['config_page']             = 'Page';
