7de44d50a6ff259a01a1959f7084039ff3006aed
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-07-10 16:06:13 +0200

    Updated Polish (pl) translation from Transifex

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -14,3 +14,16 @@ $lang['themes']               = 'Motywy';
 $lang['expand_container']     = 'Rozwiń/Zwiń';
 $lang['administrative_tasks'] = 'Zadania administracyjne';
 $lang['additional_plugins']   = 'Dodatkowe wtyczki';
+
+// Configuration Manager Template Sections
+$lang['config_theme']            = 'Motyw';
+$lang['config_sidebar']          = 'Pasek boczny';
+$lang['config_navbar']           = 'Pasek nawigacyjny';
+$lang['config_semantic']         = 'Semantyka';
+$lang['config_layout']           = 'Układ';
+$lang['config_toc']              = 'Spis treści';
+$lang['config_discussion']       = 'Dyskusja';
+$lang['config_cookie_law']       = 'Polityka plików cookies';
+$lang['config_google_analytics'] = 'Google Analytics';
+$lang['config_browser_title']    = 'Tytuł w przeglądarce';
+$lang['config_page']             = 'Strona';
