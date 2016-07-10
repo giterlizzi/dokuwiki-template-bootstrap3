104e763bb471c3779fb11c2ba3bee550282d38cb
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-07-10 16:06:12 +0200

    Updated Korean (ko) translation from Transifex

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -14,3 +14,16 @@ $lang['themes']               = '테마';
 $lang['expand_container']     = '펼치기/접기';
 $lang['administrative_tasks'] = '관리 작업';
 $lang['additional_plugins']   = '추가적인 플러그인';
+
+// Configuration Manager Template Sections
+$lang['config_theme']            = 'Theme';
+$lang['config_sidebar']          = 'Sidebar';
+$lang['config_navbar']           = 'Navbar';
+$lang['config_semantic']         = 'Semantic';
+$lang['config_layout']           = 'Layout';
+$lang['config_toc']              = 'TOC';
+$lang['config_discussion']       = 'Discussion';
+$lang['config_cookie_law']       = 'Cookie Law';
+$lang['config_google_analytics'] = 'Google Analytics';
+$lang['config_browser_title']    = 'Browser Title';
+$lang['config_page']             = 'Page';
