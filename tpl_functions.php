03461e1028470fbeebfb8cee144de808c6c53fb9
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-07-19 18:20:56 +0200

    Added initial support for template by namespace (thanks to @Digitalin on #230)

--- tpl_functions.php
+++ tpl_functions.php
@@ -896,6 +896,16 @@ function bootstrap3_conf($key, $default = false) {
 
   switch ($key) {
 
+    case 'bootstrapTheme':
+      @list($theme, $bootswatch) = bootstrap3_theme_by_namespace();
+      if ($theme) return $theme;
+      return $value;
+
+    case 'bootswatchTheme':
+      @list($theme, $bootswatch) = bootstrap3_theme_by_namespace();
+      if ($bootswatch) return $bootswatch;
+      return $value;
+
     case 'showTools':
     case 'showSearchForm':
     case 'showPageTools':
@@ -1782,5 +1792,29 @@ function bootstrap3_content($content) {
   $content = str_replace('<div class="table ', '<div class="', $content);
 
   return $content;
+
+}
+
+
+
+function bootstrap3_theme_by_namespace() {
+
+  global $ID;
+
+  $themes_filename = DOKU_CONF.'bootstrap3.themes.conf';
+
+  if (file_exists($themes_filename)) {
+
+    $config = confToHash($themes_filename);
+    krsort($config);
+
+    foreach ($config as $page => $theme) {
+      if (preg_match("/^$page/", $ID)) {
+        return split('/', $theme);
+      }
+    }
+  }
+
+  return array();
 
 }
