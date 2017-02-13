8aa65db36a8bdbcf46d08b280c48bc17375f6077
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2017-02-13 11:26:59 +0100

    Updated Portuguese (Brazil) (pt-br) translation from Transifex

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -7,13 +7,17 @@
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
-$lang['discussion']           = 'Discussão';
-$lang['back_to_article']      = 'Voltar ao artigo';
-$lang['userpage']             = 'Página do usuário';
-$lang['themes']               = 'Temas';
-$lang['expand_container']     = 'Expandir/Reduzir';
-$lang['administrative_tasks'] = 'Tarefas Administrativas';
-$lang['additional_plugins']   = 'Plugins Adicionais';
+$lang['discussion']              = 'Discussão';
+$lang['back_to_article']         = 'Voltar ao artigo';
+$lang['userpage']                = 'Página do usuário';
+$lang['themes']                  = 'Temas';
+$lang['expand_container']        = 'Expandir/Reduzir';
+$lang['administrative_tasks']    = 'Tarefas Administrativas';
+$lang['additional_plugins']      = 'Plugins Adicionais';
+$lang['share_on']                = 'Share on';
+$lang['send_mail']               = 'Send e-Mail';
+$lang['print']                   = 'Print';
+$lang['add_new_page']            = 'Add New Page';
 
 // Configuration Manager Template Sections
 $lang['config_theme']            = 'Theme';
@@ -27,3 +31,4 @@ $lang['config_cookie_law']       = 'Cookie Law';
 $lang['config_google_analytics'] = 'Google Analytics';
 $lang['config_browser_title']    = 'Browser Title';
 $lang['config_page']             = 'Page';
+
