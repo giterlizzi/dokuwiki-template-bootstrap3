f0b8d4aad6441e242e43961c015303c324f7b25f
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2017-12-27 23:28:22 +0100

    Added fallback mb_detect_encoding function

--- inc/simple_html_dom.php
+++ inc/simple_html_dom.php
@@ -1,5 +1,34 @@
 <?php
 
+
+if ( ! function_exists('mb_detect_encoding') ) {
+
+  function mb_detect_encoding ($string, $enc=null, $ret=null) {
+
+    $enclist = array(
+      'UTF-8', 'ASCII',
+      'ISO-8859-1', 'ISO-8859-2', 'ISO-8859-3', 'ISO-8859-4', 'ISO-8859-5',
+      'ISO-8859-6', 'ISO-8859-7', 'ISO-8859-8', 'ISO-8859-9', 'ISO-8859-10',
+      'ISO-8859-13', 'ISO-8859-14', 'ISO-8859-15', 'ISO-8859-16',
+      'Windows-1251', 'Windows-1252', 'Windows-1254',
+    );
+
+    $result = false;
+
+    foreach ($enclist as $item) {
+      $sample = iconv($item, $item, $string);
+      if (md5($sample) == md5($string)) {
+        if ($ret === NULL) { $result = $item; } else { $result = true; }
+        break;
+      }
+    }
+
+    return $result;
+  }
+
+}
+
+
 /**
  * Website: http://sourceforge.net/projects/simplehtmldom/
  * Acknowledge: Jose Solorzano (https://sourceforge.net/projects/php-html/)
