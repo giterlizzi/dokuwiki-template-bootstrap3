3f8b5e473aa788d883d0c78d583aba642c8baef0
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2018-01-03 18:50:26 +0100

    Added Indonesian (id) translation from Transifex

---
+++ lang/id/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Indonesian Language file for template
+ *
+ * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @author   Arif Budiman
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['discussion']              = 'Diskusi';
+$lang['back_to_article']         = 'Kembali ke artikel';
+$lang['userpage']                = 'Halaman pengguna';
+$lang['themes']                  = 'Tema';
+$lang['expand_container']        = 'Perluas/Persempit';
+$lang['administrative_tasks']    = 'Tugas Administratif';
+$lang['additional_plugins']      = 'Plugin Tambahan';
+$lang['share_on']                = 'Bagikan pada';
+$lang['send_mail']               = 'Kirim e-Mail';
+$lang['print']                   = 'Cetak';
+$lang['add_new_page']            = 'Tambah Halaman Baru';
+
+// Configuration Manager Template Sections
+$lang['config_theme']            = 'Tema';
+$lang['config_sidebar']          = 'Bilah Sisi';
+$lang['config_navbar']           = 'Bilah Nav';
+$lang['config_semantic']         = 'Semantik';
+$lang['config_layout']           = 'Tata Latak';
+$lang['config_toc']              = 'Daftar Isi';
+$lang['config_discussion']       = 'Diskusi';
+$lang['config_cookie_law']       = 'Aturan Cookie';
+$lang['config_google_analytics'] = 'Google Analytics';
+$lang['config_browser_title']    = 'Judul Peramban';
+$lang['config_page']             = 'Halaman';
+
