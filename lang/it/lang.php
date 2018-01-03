1dcccab1ed73ac266d75920af45f268ad3ebdb33
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2018-01-03 18:50:26 +0100

    Updated Italian (it) translation from Transifex

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -6,23 +6,28 @@
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
-$lang['discussion']           = 'Discussione';
-$lang['back_to_article']      = 'Ritorna all\'articolo';
-$lang['userpage']             = 'Pagina Utente';
-$lang['themes']               = 'Temi';
-$lang['expand_container']     = 'Espandi/Riduci';
-$lang['administrative_tasks'] = 'Operazioni di Amministrazione';
-$lang['additional_plugins']   = 'Plugin Aggiuntivi';
+$lang['discussion']              = 'Discussione';
+$lang['back_to_article']         = 'Ritorna all\'articolo';
+$lang['userpage']                = 'Pagina Utente';
+$lang['themes']                  = 'Temi';
+$lang['expand_container']        = 'Espandi/Riduci';
+$lang['administrative_tasks']    = 'Operazioni di Amministrazione';
+$lang['additional_plugins']      = 'Plugin Aggiuntivi';
+$lang['share_on']                = 'Condividi su';
+$lang['send_mail']               = 'Invia e-Mail';
+$lang['print']                   = 'Stampa';
+$lang['add_new_page']            = 'Aggiungi una nuova pagina';
 
 // Configuration Manager Template Sections
-$lang['config_theme']            = 'Theme';
+$lang['config_theme']            = 'Tema';
 $lang['config_sidebar']          = 'Sidebar';
-$lang['config_navbar']           = 'Navbar';
-$lang['config_semantic']         = 'Semantic';
+$lang['config_navbar']           = 'Barra di nagivazione';
+$lang['config_semantic']         = 'Semantica';
 $lang['config_layout']           = 'Layout';
-$lang['config_toc']              = 'TOC';
-$lang['config_discussion']       = 'Discussion';
+$lang['config_toc']              = 'Indice';
+$lang['config_discussion']       = 'Discussione';
 $lang['config_cookie_law']       = 'Cookie Law';
 $lang['config_google_analytics'] = 'Google Analytics';
-$lang['config_browser_title']    = 'Browser Title';
-$lang['config_page']             = 'Page';
+$lang['config_browser_title']    = 'Titolo del browser';
+$lang['config_page']             = 'Pagina';
+
