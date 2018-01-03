1244b395923e59038bb33e1e89a7d88eb7bacf7b
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2018-01-03 18:50:26 +0100

    Added Persian (fa) translation from Transifex

---
+++ lang/fa/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Persian Language file for template
+ *
+ * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @author   Shevin Shiasi
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['discussion']              = 'بحث';
+$lang['back_to_article']         = 'بازگشت به مقاله';
+$lang['userpage']                = 'صفحه کاربر';
+$lang['themes']                  = 'پوسته‌ها';
+$lang['expand_container']        = 'گشودن/بستن';
+$lang['administrative_tasks']    = 'وظایف مدیریتی';
+$lang['additional_plugins']      = 'افزونه‌های اضافی';
+$lang['share_on']                = 'اشتراک گذاری در';
+$lang['send_mail']               = 'فرستادن ایمیل';
+$lang['print']                   = 'چاپ';
+$lang['add_new_page']            = 'افزودن صفحه جدید';
+
+// Configuration Manager Template Sections
+$lang['config_theme']            = 'پوسته';
+$lang['config_sidebar']          = 'نوارکناری';
+$lang['config_navbar']           = 'نوار پیمایش';
+$lang['config_semantic']         = 'معنایی';
+$lang['config_layout']           = 'طرح';
+$lang['config_toc']              = 'TOC';
+$lang['config_discussion']       = 'بحث';
+$lang['config_cookie_law']       = 'قانون کوکی';
+$lang['config_google_analytics'] = 'Google Analytics';
+$lang['config_browser_title']    = 'عنوان مرورگر';
+$lang['config_page']             = 'صفحه';
+
