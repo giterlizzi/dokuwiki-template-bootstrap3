a1052766bae18a600a8f2d4068b6ae4d02eb03b9
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2018-01-03 18:50:26 +0100

    Updated Japanese (ja) translation from Transifex

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -7,13 +7,17 @@
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
-$lang['discussion']           = 'コメント';
-$lang['back_to_article']      = '記事へ戻る';
-$lang['userpage']             = 'ユーザーページ';
-$lang['themes']               = 'テーマ';
-$lang['expand_container']     = '拡大・縮小';
-$lang['administrative_tasks'] = '管理タスク';
-$lang['additional_plugins']   = '追加プラグイン';
+$lang['discussion']              = 'コメント';
+$lang['back_to_article']         = '記事へ戻る';
+$lang['userpage']                = 'ユーザーページ';
+$lang['themes']                  = 'テーマ';
+$lang['expand_container']        = '拡大・縮小';
+$lang['administrative_tasks']    = '管理タスク';
+$lang['additional_plugins']      = '追加プラグイン';
+$lang['share_on']                = '共有';
+$lang['send_mail']               = 'メール送信';
+$lang['print']                   = '印刷';
+$lang['add_new_page']            = '新規ページの追加';
 
 // Configuration Manager Template Sections
 $lang['config_theme']            = 'テーマ';
@@ -27,3 +31,4 @@ $lang['config_cookie_law']       = 'Cookie Law';
 $lang['config_google_analytics'] = 'Google アナリティクス';
 $lang['config_browser_title']    = 'ブラウザタイトル';
 $lang['config_page']             = 'ページ';
+
