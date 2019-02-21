d0ec56d2990f52158165fe98a1246fc482b9f137
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2019-02-22 00:54:17 +0100

    Added Esperanto (eo) translation from Transifex

---
+++ lang/eo/lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Esperanto Language file for template
+ *
+ * @author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @author   Aleksej Kuznetsov
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['discussion']              = 'Diskuto';
+$lang['back_to_article']         = 'Back to article';
+$lang['userpage']                = 'Uzanta pagxo';
+$lang['purge_cache_page']        = 'Purge page cache';
+$lang['themes']                  = 'Temoj';
+$lang['expand_container']        = 'Expand/Reduce';
+$lang['administrative_tasks']    = 'Administrative Tasks';
+$lang['additional_plugins']      = 'Additional Plugins';
+$lang['share_on']                = 'Share on';
+$lang['send_mail']               = 'Sendi retposxton';
+$lang['print']                   = 'Printi';
+$lang['permalink']               = 'Permalink';
+$lang['add_new_page']            = 'Aldoni novan pagxon';
+
+// Configuration Manager Template Sections
+$lang['config_theme']            = 'Temo';
+$lang['config_sidebar']          = 'Sidebar';
+$lang['config_navbar']           = 'Navbar';
+$lang['config_semantic']         = 'Semantic';
+$lang['config_layout']           = 'Layout';
+$lang['config_toc']              = 'TOC';
+$lang['config_discussion']       = 'Diskuto';
+$lang['config_avatar']           = 'Avatar';
+$lang['config_cookie_law']       = 'Cookie Law';
+$lang['config_google_analytics'] = 'Google Analytics';
+$lang['config_browser_title']    = 'Browser Title';
+$lang['config_page']             = 'Pagxo';
+
