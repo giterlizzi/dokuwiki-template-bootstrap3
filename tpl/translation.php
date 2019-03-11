0f51da3ead94bdbfebdb2c25004ed4e927801f73
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2019-03-12 00:16:34 +0100

    Added ":usermenu" hook and some optimizations

--- tpl/translation.php
+++ tpl/translation.php
@@ -32,12 +32,13 @@ if (! $translation->istranslatable($ID)) return false;
         <a href="<?php wl($ID) ?>" class="dropdown-toggle" data-target="#" data-toggle="dropdown" title="<?php echo $trans_label ?>" role="button" aria-haspopup="true" aria-expanded="false">
             <i class="fa fa-fw fa-flag"></i> <span class="hidden-lg hidden-md hidden-sm"><?php echo $trans_label ?></span><span class="caret"></span>
         </a>
-    <ul class="dropdown-menu" role="menu">
+        <ul class="dropdown-menu" role="menu">
             <li class="dropdown-header hidden-xs hidden-sm">
                 <i class="fa fa-fw fa-flag"></i> <?php echo $trans_label ?>
             </li>
             <?php echo $trans_items ?>
-    </ul>
+        </ul>
+    </li>
 </ul>
 <!-- /translation -->
 <?php endif; ?>
