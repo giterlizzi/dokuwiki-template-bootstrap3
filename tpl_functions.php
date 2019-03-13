007465d151ff36b588f507b15eb42c2313448382
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2019-03-13 10:02:48 +0100

    Added compatibility PHP script for Bootstrap Wrapper and Icons plugin

---
+++ tpl_functions.php
@@ -0,0 +1,5 @@
+<?php
+
+// Fallback script for compatibility with Bootstrap Wrapper and Icons plugins
+
+@require_once(dirname(__FILE__).'/tpl/functions.php');  // include hook for template functions
---
+++ tpl_global.php
@@ -0,0 +1,5 @@
+<?php
+
+// Fallback script for compatibility with Bootstrap Wrapper and Icons plugins
+
+@include_once(dirname(__FILE__).'/tpl/global.php');      // Include template global variables
