237795e107b82ecf558b708a0000c523ad604744
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2019-06-03 00:46:51 +0200

    Fixed compatibility for Folded plugin (thanks to @virk on #422)

---
+++ js/plugins/folded.js
@@ -0,0 +1,17 @@
+/*
+ * DokuWiki Bootstrap3 Template: Plugins Hacks!
+ *
+ * Home     http://dokuwiki.org/template:bootstrap3
+ * Author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * License  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+// Folded Plugin
+
+var $folded = jQuery('.fold_unfold_all_new');
+
+if ($folded.length) {
+
+  $folded.addClass('menuitem');
+
+}
