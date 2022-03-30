e99d49e1f39df382ffed51f99b36a74196074c58
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2022-03-30 22:56:00 +0200

    Improved icon in Form Events
    #552

--- script.js
+++ script.js
@@ -152,6 +152,19 @@ var dw_template = {
         // Toolbar
         jQuery('#tool__bar').addClass('btn-group');
 
+        // Add icons
+        jQuery('[data-dw-icon]').each(function () {
+            var $self = jQuery(this);
+            var $icon = jQuery('<span class="iconify mr-1">').attr('data-icon', $self.attr('data-dw-icon'));
+
+            if ($self.attr('data-dw-icon-target')) {
+                $self.find($self.attr('data-dw-icon-target')).prepend($icon);
+            } else {
+                $self.prepend($icon);
+            }
+
+        });
+
         // Picker
         //     if (dw_mode('edit')) {
         //       jQuery('.picker').addClass('btn-group');
